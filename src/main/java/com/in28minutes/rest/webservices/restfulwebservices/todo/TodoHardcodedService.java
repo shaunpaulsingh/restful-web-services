7f903335f556f7bc3b95bf428d3fefa4844e5094
Shaun Paul Singh <shaunpaulsingh@gmail.com>
2022-04-20 08:17:21 +0800

    Added more endpoints

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
@@ -1,4 +1,5 @@
 package com.in28minutes.rest.webservices.restfulwebservices.todo;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -19,6 +20,17 @@ public class TodoHardcodedService {
 
 	public List<Todo> findAll() {
 		return todos;
+	}
+	
+	public Todo save(Todo todo) {
+		if(todo.getId()==-1 || todo.getId()==0) {
+			todo.setId(++idCounter);
+			todos.add(todo);
+		} else {
+			deleteById(todo.getId());
+			todos.add(todo);
+		}
+		return todo;
 	}
 
 	public Todo deleteById(long id) {
